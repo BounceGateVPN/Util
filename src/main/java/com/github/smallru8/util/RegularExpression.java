f03aa622c1fc662b43ad99c6c0282bc53a206dca
smallru8 <30855604+smallru8@users.noreply.github.com>
2020-11-23 17:28:17 +0800

    Add regular expression : isIPAddress

--- src/main/java/com/github/smallru8/util/RegularExpression.java
+++ src/main/java/com/github/smallru8/util/RegularExpression.java
@@ -22,4 +22,17 @@ public class RegularExpression {
 		return str.matches("[A-Za-z0-9]*");
 	}
 	
+	public static boolean isIPAddress(String str) {
+		String[] ip4 = str.split("\\.");
+		if(ip4.length!=4)
+			return false;
+		for(int i=0;i<4;i++) {
+			if(!isDigitOnly(ip4[i]))
+				return false;
+			if(Integer.parseInt(ip4[i])>255||Integer.parseInt(ip4[i])<0)
+				return false;
+		}
+		return true;
+	}
+	
 }
